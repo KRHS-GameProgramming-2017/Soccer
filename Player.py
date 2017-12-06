057b30a04f838bf5584d4b10a117601c1cd65a56
Noah <noah.gray@kearsarge.org>
2017-12-06 13:55:58 -0500

    Playerball

--- Player.py
+++ Player.py
@@ -1,22 +1,39 @@
 import pygame, math
 
 class playerball();
- def __init__(self,  screenSize, size=None):
-        self.image = pygame.image.load("Images/ball/Soccerball.png")
+ def __init__(self, side, screenSize, size=None):
+        self.side = side
+        if self.side == "right":
+            self.image = pygame.image.load("Images/player/player1.png")
+        else:
+            self.image = pygame.image.load("Images/player/player2.png")
         if size:
             self.image=pygame.transform.scale(self.image,size)
-        self.rect = self.image.get_rect(center=[screenSize[0]/2, screenSize[1]/2])
+        
+        if self.side == "right":
+            self.rect = self.image.get_rect(center=[screenSize[0]*.85, screenSize[1]/2])
+        else:
+            self.rect = self.image.get_rect(center=[screenSize[0]*.15, screenSize[1]/2])
+            
         self.radius=self.rect.width/2
         self.speed = [0,0]
         self.maxSpeed = 10
         self.boostedSpeed = 13
+        self.borderSize = 10
+        self.endSize = 33
+        self.centerSize = 12/2
         
 def wallBounce(self,size):
         width=size[0]
         height=size[1]
         
-        if self.rect.left < 0 or self.rect.right > width:
-            self.speed[0] = -self.speed[0]
+        if self.side == "right":
+            if self.rect.left < width/2+self.centerSize or self.rect.right > width-endSize:
+                self.speed[0] = -self.speed[0]
+        else:
+            if self.rect.left < self.endSize or self.rect.right > width/2-self.centersize:
+                self.speed[0] = -self.speed[0]
+
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] = -self.speed[1]
      
