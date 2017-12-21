6f4711c145064145ed899e8070822fa2b3582ea3
Noah <noah.gray@kearsarge.org>
2017-12-21 13:34:34 -0500

    start of front screen

--- main.py
+++ main.py
@@ -38,74 +38,76 @@ lScore = Scoreboard([width/2-25, 25], "left")
 
 
 
-
-
+mode = "game"
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                p1.go("up")
-            if event.key == pygame.K_DOWN:
-                p1.go("down")
-            if event.key == pygame.K_LEFT: 
-                p1.go("left")
-            if event.key == pygame.K_RIGHT: 
-                p1.go("right")
-            if event.key == pygame.K_w:
-                p2.go("up")
-            if event.key == pygame.K_s:
-                p2.go("down")
-            if event.key == pygame.K_a:    
-                p2.go("left")
-            if event.key == pygame.K_d:  
-                p2.go("right")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP: 
-                p1.go("stop up")
-            if event.key == pygame.K_DOWN: 
-                p1.go("stop down")
-            if event.key == pygame.K_LEFT: 
-                p1.go("stop left")
-            if event.key == pygame.K_RIGHT: 
-                p1.go("stop right")
-            if event.key == pygame.K_w:
-                p2.go("stop up")
-            if event.key == pygame.K_s:
-                p2.go("stop down")
-            if event.key == pygame.K_a: 
-                p2.go("stop left")
-            if event.key == pygame.K_d:   
-                p2.go("stop right")    
-    
-    p1.move()
-    p1.wallBounce()
-    p2.move()
-    p2.wallBounce()
-    
-    ball.move()
-    ball.wallBounce(size)
-    ball.playerBounce(p1)
-    ball.playerBounce(p2)
-    
-    if ball.bounceGoal(lGoal, size):
-        p1.reset()
-        p2.reset()
-    if ball.bounceGoal(rGoal, size):
-        p1.reset()
-        p2.reset()
+    while mode == "menu":
+        pass
+    while mode == "game" :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    p1.go("up")
+                if event.key == pygame.K_DOWN:
+                    p1.go("down")
+                if event.key == pygame.K_LEFT: 
+                    p1.go("left")
+                if event.key == pygame.K_RIGHT: 
+                    p1.go("right")
+                if event.key == pygame.K_w:
+                    p2.go("up")
+                if event.key == pygame.K_s:
+                    p2.go("down")
+                if event.key == pygame.K_a:    
+                    p2.go("left")
+                if event.key == pygame.K_d:  
+                    p2.go("right")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP: 
+                    p1.go("stop up")
+                if event.key == pygame.K_DOWN: 
+                    p1.go("stop down")
+                if event.key == pygame.K_LEFT: 
+                    p1.go("stop left")
+                if event.key == pygame.K_RIGHT: 
+                    p1.go("stop right")
+                if event.key == pygame.K_w:
+                    p2.go("stop up")
+                if event.key == pygame.K_s:
+                    p2.go("stop down")
+                if event.key == pygame.K_a: 
+                    p2.go("stop left")
+                if event.key == pygame.K_d:   
+                    p2.go("stop right")    
+        
+        p1.move()
+        p1.wallBounce()
+        p2.move()
+        p2.wallBounce()
+        
+        ball.move()
+        ball.wallBounce(size)
+        ball.playerBounce(p1)
+        ball.playerBounce(p2)
+        
+        if ball.bounceGoal(lGoal, size):
+            p1.reset()
+            p2.reset()
+        if ball.bounceGoal(rGoal, size):
+            p1.reset()
+            p2.reset()
 
-    bgColor = r,g,b
-    screen.fill(bgColor)
-    screen.blit(bgImage, bgRect)
-    screen.blit(ball.image, ball.rect)
-    screen.blit(rGoal.image, rGoal.rect)
-    screen.blit(lGoal.image, lGoal.rect)
-    screen.blit(p1.image, p1.rect)
-    screen.blit(p2.image, p2.rect)
-    screen.blit(rScore.image, rScore.rect)
-    screen.blit(lScore.image, lScore.rect)
-    pygame.display.flip()
-    clock.tick(60)
-    print clock.get_fps()
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        screen.blit(ball.image, ball.rect)
+        screen.blit(rGoal.image, rGoal.rect)
+        screen.blit(lGoal.image, lGoal.rect)
+        screen.blit(p1.image, p1.rect)
+        screen.blit(p2.image, p2.rect)
+        screen.blit(rScore.image, rScore.rect)
+        screen.blit(lScore.image, lScore.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        print clock.get_fps()
